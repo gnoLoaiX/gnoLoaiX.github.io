eb63f8ed46255a36f55c9aa719db56dd982ca3b7
HelloYu <2018911454@qq.com>
2018-09-18 23:30:08 +0800

    My works

--- js/demo.js
+++ js/demo.js
@@ -1,3 +1,3 @@
 setTimeout(function() {
-	alert("This is my first test website. My blog and GitHub can be accessed normally. Mailbox contact me without processing server side, You can contact my QQ:2018911454@qq.com.");
+	alert("Notice: This is my first test website. My blog and GitHub can be accessed normally. Mailbox contact me without processing server side, You can contact my QQ:2018911454@qq.com.");
 },1000);
