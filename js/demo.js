0671cafe0095702a33ff0f54d55b979a66752e8e
HelloYu <2018911454@qq.com>
2018-09-21 19:59:52 +0800

    demo

--- js/demo.js
+++ js/demo.js
@@ -1,3 +1,3 @@
 setTimeout(function() {
-	alert("Notice: This is my first test website. My Blog and Github can be accessed normally. Mailbox contact me without processing server side, You can contact my QQ:2018911454@qq.com.");
+	alert("Notice: This is my first test website. My Blog and Github can be accessed normally. Mailbox contact me without processing server side, You can contact me:fast13469nd@gmail.com");
 },1000);
