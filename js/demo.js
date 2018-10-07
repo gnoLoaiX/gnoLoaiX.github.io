841e2f261de2f72760e195205c417fa133068902
HelloYu <2018911454@qq.com>
2018-10-07 18:27:38 +0800

    demo js

--- js/demo.js
+++ js/demo.js
@@ -1,3 +1,3 @@
-setTimeout(function() {
-	alert("注意: This is my first test website. My Blog and Github can be accessed normally. Mailbox contact me without processing server side, You can contact me:fast13469nd@gmail.com");
-},1000);
+// setTimeout(function() {
+// 	alert("注意: This is my first test website. My Blog and Github can be accessed normally. Mailbox contact me without processing server side, You can contact me:fast13469nd@gmail.com");
+// },1000);
